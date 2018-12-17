6f1b57ec86df08c3bdcbdf1dacf209e9f2fb84c7
Patricia <martinperez.patricia@gmail.com>
2018-12-17 02:58:19 +0100

    fin proyecto

---
+++ ColaDefinitiva/Cola.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ColaDefinitiva
+{
+    public class Cola<T>
+    {
+        public Cola()
+        {
+			this.elementos = new List<T>();
+			
+        }
+
+		public void Add(T x){
+			elementos.Add(x);
+		}
+
+		public int Longitud{
+			get{
+				return this.elementos.Count;
+			}
+		}
+
+		public T Principio{
+			get{
+				return this.elementos[0];
+			}
+		}
+
+		public T Final{
+			get{
+				return this.elementos[this.elementos.Count - 1];
+			}
+		}
+
+		public bool EstaVacia{
+			get{
+				return this.Longitud == 0;
+			}
+		}
+
+		public void Elimina(){
+			this.elementos.Clear();
+		}
+
+		List<T> elementos;
+
+    }
+}
---
+++ Tests/TestCola.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Tests
+{
+	using NUnit.Framework;
+	using ColaDefinitiva;
+
+    [TestFixture] //Clase que contiene metodos de pruebas
+    public class TestCola
+    {
+		[Test] //metodo de test
+		public void TestCreaCola()
+        {
+			var cola = new Cola<int>();
+
+			cola.Add(1);
+			cola.Add(2);
+
+			Assert.AreEqual( 2, cola.Longitud );
+
+			while(!cola.EstaVacia){
+				cola.Elimina();
+			}
+
+			Assert.AreEqual(0, cola.Longitud);
+        
+		}
+
+        [Test]
+		public void TestFinal(){
+			var cola = new Cola<int>();
+
+            cola.Add(1);
+			Assert.AreEqual(1, cola.Final);
+            cola.Add(2);
+			Assert.AreEqual(2, cola.Final);
+		}
+    }
+}
